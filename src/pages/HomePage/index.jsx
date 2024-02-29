51d8b84936c5d1eedd7fa2b15a546cc0673a6ed0
GalangYuhu <galangmrd09@gmail.com>
2024-02-29 22:50:06 +0700

    feat : add to file homePage.jsx from folder pages : authot galang

---
+++ src/pages/HomePage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Navbars from '../../components/NavbarSeconds/Navbar'
+
+
+const IndexRoot = () => {
+  return (
+    <div>
+        <Navbars />
+    </div>
+  )
+}
+
+export default IndexRoot
