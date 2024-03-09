086ed0ac5851438ea05f43dc5e4f8326d2d586fb
GalangYuhu <galangmrd09@gmail.com>
2024-03-10 03:11:35 +0700

    done

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Navbars from '../../components/NavbarSeconds/Navbar'
 import CardSlider from '../../components/cardSlider/CardSlider'
+import ProductCatalog from '../../components/productCatalog/productCatalog'
 
 const IndexRoot = () => {
   return (
     <div>
         <Navbars />
         <CardSlider />
+        <ProductCatalog />
     </div>
   )
 }
