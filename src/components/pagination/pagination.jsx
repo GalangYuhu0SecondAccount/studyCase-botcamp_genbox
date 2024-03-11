c9b904c74be1cd866278bcef4b8891b980ed92ee
GalangYuhu <galangmrd09@gmail.com>
2024-03-11 14:39:17 +0700

    added file pagination

---
+++ src/components/pagination/pagination.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+
+
+const Pagination = ({postPerPage , length , handlePagination , currentPage}) => {
+    let PaginationNumber = []
+
+
+    for (let i = 1; i <= Math.ceil( length / postPerPage );  i++) {
+        PaginationNumber.push(i);
+        
+    }
+
+
+    return (
+        <>
+        <div className="pagination">
+            {
+                PaginationNumber.map((data) => (
+                    <button key={data} onClick={() => handlePagination(data)} className={currentPage === data ? "active" : ""}>
+                        {data}
+                    </button>
+                ))
+            }
+        </div>
+        </>
+    )
+}
+
+
+export default Pagination;
